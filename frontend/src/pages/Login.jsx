9adf409edd697a8190617d11ec0a8451635ea00a
Kushal <nagakushal10@gmail.com>
2025-02-04 22:47:57 +0530

    Frontend updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,25 +13,34 @@ const Login = () => {
   return (
     <form className="min-h-[80vh] flex items-center" onSubmit={handleSubmit}>
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
-        <p>{state==="SignUp" ? "Create Account" : "Login to your Account"}</p>
-        <p>{state==="SignUp" ? "Please sign up to book appointment" : "Please login to book appointment"}</p>
+        <p className="text-3xl font-semibold text-primary px-20.5">{state==="SignUp" ? "Create Account" : "Login Account"}</p>
+        <p className="px-19 font-medium mb-4">{state==="SignUp" ? "Please sign up to book appointment" : "Please login to book appointment"}</p>
 
-        <div>
-          <p>Full Name</p>
-          <input type="text" placeholder="Enter your full name..." value={name} onChange={(e)=> setName(e.target.value)}/>
-        </div>
+        {
+          state==="SignUp" && (
+            <div className="w-full">
+              <p>Full Name</p>
+              <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="text" placeholder="Enter your full name..." value={name} onChange={(e)=> setName(e.target.value)}/>
+            </div>
+          )
+        }
 
-        <div>
+        <div className="w-full">
           <p>Email</p>
-          <input type="email" placeholder="Enter your email..." value={email} onChange={(e)=> setEmail(e.target.value)}/>
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="email" placeholder="Enter your email..." value={email} onChange={(e)=> setEmail(e.target.value)}/>
         </div>
 
-        <div>
+        <div className="w-full">
           <p>Password</p>
-          <input type="password" placeholder="Enter your password..." value={password} onChange={(e)=> setPassword(e.target.value)}/>
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="password" placeholder="Enter your password..." value={password} onChange={(e)=> setPassword(e.target.value)}/>
         </div>
 
-        <button>{state==="SignUp" ? "Sign Up" : "Login"}</button>
+        <button className="bg-[#5f6FFF] text-white w-full py-2 rounded-md text-base mb-1.5">{state==="SignUp" ? "Sign Up" : "Login"}</button>
+        {
+          state==="SignUp" 
+          ? <p onClick={()=> setState("Login")}>Already have an account? <span className="text-primary underline cursor-pointer">Login here</span></p>
+          : <p onClick={()=> setState("SignUp")}>New to Prescripto? <span className="text-primary underline cursor-pointer">Click here</span></p>
+        }
       </div>
     </form>
   )
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+import { assets } from "../assets/assets_frontend/assets";
+
 const MyProfile = () => {
+  const [userData, setUserData]=useState({
+    name: "Naga Kushal Vankadara",
+    image: assets.profile_pic,
+    email: "kushal123@gmail.com",
+    phone: "+91 9876543210",
+    address: {
+      line1: "Road no 57, King market",
+      line2: "One town, Kurnool"
+    },
+    gender: "Male",
+    dob: "2000-01-20"
+  });
+
+  const [isEdit, setIsEdit]=useState(false);
+
   return (
-    <div>MyProfile</div>
+    <div>
+      <img src={userData.image} alt="user"/>
+    </div>
   )
 }
 
